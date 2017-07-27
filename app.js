8968a1729d1d390495280ac046bc1155f7d08ef5
Ruhull Alam <Ruhull.alam@outlook.com>
2017-07-27 12:36:23 +0100

    command line options added

--- app.js
+++ app.js
@@ -6,39 +6,56 @@ const yargs = require('yargs')
 
 const notes = require('./notes.js')
 
-const argv = yargs.argv
-let command = argv._[0]
-console.log('Command: '+command.toLowerCase())
+const titleOptions = {
+  describe: 'The title of the note',
+  demand: true,
+  alias : 't'
+}
+
+const bodyOptions = {
+  describe: 'The body of the note',
+  demand: true,
+  alias: 'b'
+}
+
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'List all existing notes')
+  .command('read', 'Read a note', {
+    title: titleOptions
+  }).
+  command('remove', 'Remove an exisitng note', {
+    title : titleOptions
+  })
+  .help()
+  .argv
+let command = argv._[0].toLowerCase()
+
+console.log('Command: '+ command)
 console.log('-------------')
 
-if (command.toLowerCase() === 'add') {
+if (command === 'add') {
   var note = notes.addNote(argv.title, argv.body)
 
-  if (note) {
-    notes.logNotes(note)
-  } else {
-    console.log('Note already exists')
-  }
+  note ? notes.logNotes(note) : console.log('Note already exists')
 
-} else if (command.toLowerCase() === 'list') {
+} else if (command === 'list') {
     var allNotes = notes.getAll()
 
     allNotes.forEach((note) => {
       notes.logNotes(note)
     })
-
   console.log('-----end of notes-----')
 
-} else if (command.toLowerCase() === 'read') {
+} else if (command === 'read') {
     var note = notes.getNote(argv.title)
 
-    if (note) {
-      notes.logNotes(note)
-    } else {
-      console.log('Note doesn\'t exist')
-    }
+    note ? notes.logNotes(note) : console.log('Note doesn\'t exist')
 
-} else if (command.toLowerCase() === 'remove') {
+} else if (command === 'remove') {
     var note = notes.removeNote(argv.title)
     console.log(note ? 'Note Removed' : 'Note not found')
 }
